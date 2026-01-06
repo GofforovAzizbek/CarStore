5c022ea6d81f11c451d99cf995b7c873cff7867e
GofforovAzizbek <gofforovazizbek2024@gmail.com>
2026-01-06 23:05:12 +0500

    Update

---
+++ ObjectLesson/pdfTwo.js
@@ -0,0 +1,34 @@
+// 1-
+// const Animal = {
+//   speak() {
+//     console.log("Animal speaks");
+//   }
+// };
+
+// const Dog = Object.create(Animal);
+// Dog.bark = function () {
+//   console.log("Woof!");
+// };
+
+// Dog.speak();
+// Dog.bark();
+
+// 2-
+// const Animal = {
+//   speak() {
+//     console.log("Animal speaks");
+//   }
+// };
+
+// const Dog2 = Object.create(Animal);
+
+// Dog2.speak = function () {
+//   console.log("Dog barks");
+// };
+
+// Animal.speak();
+// Dog2.speak();
+
+// 3-
+
+masala shartlariga tushunmadim hullas
